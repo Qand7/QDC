e72828e944ee62d82fc3cd76e68d2e2aa98986e8
Qandy <145124536+Qand7@users.noreply.github.com>
2025-12-09 21:53:03 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create DC directory
+mkdir ~/QDCupdater/Discord/
+
+# Create Updater
+sudo cp ~/QDCupdater/qDcUpdater /usr/bin/qDcUpdater
+sudo chmod +x /usr/bin/qDcUpdater
+
+echo
+echo "---------------------------------------"
+echo "Updater installed"
+echo "Type \"qDcUpdater -h/--help\" for help"
