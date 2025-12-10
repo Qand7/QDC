b4b26ff23754ced7c0370e74e36302ff60add180
Qandy <145124536+Qand7@users.noreply.github.com>
2025-12-10 10:35:28 +0100

    Add Discord installation commands to install.sh
    Added commands to create .desktop and .exe files for Discord.

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 sudo cp ~/QDCupdater/qDcUpdater /usr/bin/qDcUpdater
 sudo chmod +x /usr/bin/qDcUpdater
 
+# Create .desktop and .exe files
+sudo cp ~/QDCupdater/Discord/discord.desktop /usr/share/applications/discord.desktop
+sudo cp ~/QDCupdater/Discord/Discord /usr/bin/
+sudo chmod +x /usr/bin/Discord
+
 echo
 echo "---------------------------------------"
 echo "Updater installed"
