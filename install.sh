cb09032a5b5ea291e25694796b4689c2adcb118c
Qandy <145124536+Qand7@users.noreply.github.com>
2026-05-25 19:17:08 +0200

    Enhance install.sh with clearer comments and icon setup
    Updated comments for clarity and added icon directory creation steps.

--- install.sh
+++ install.sh
@@ -5,10 +5,19 @@ sudo cp ~/QDCupdater/qDcUpdater /usr/bin/qDcUpdater
 sudo cp -r ~/QDCupdater /usr/share/
 sudo chmod +x /usr/bin/qDcUpdater
 
-# Create .desktop and .exe files
+# Create .desktop and executable files
 sudo cp ~/QDCupdater/Discord/discord.desktop /usr/share/applications/discord.desktop
 sudo cp ~/QDCupdater/Discord/discord /usr/bin/
 sudo chmod +x /usr/bin/discord
+
+# Why does an icon need this much work (Well it isn't work, just annoying)
+# I'm pretty sure it is much simpler than 4 lines and I am dumb
+# Prolly something like "mkdir --needed whatever /path/to/whatever"
+sudo mkdir /usr/share/icons/
+sudo mkdir /usr/share/icons/hicolor/
+sudo mkdir /usr/share/icons/hicolor/512x512
+sudo cp ~/QDCupdater/Discord/discord.png /usr/share/icons/hicolor/512x512/
+
 
 echo
 echo "---------------------------------------"
